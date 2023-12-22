6ce1e7f1972cecb8b77b43869ffd4e0748d5d9f6
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-12-22 15:41:48 +0300

    logging some activities

--- week10/app/src/main/java/com/example/week10/MainActivity.java
+++ week10/app/src/main/java/com/example/week10/MainActivity.java
@@ -11,12 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("on-create message", "created");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("start message", "started");
+        Log.d("on-start message", "started");
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("on-pause message", "paused");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("on-resume message", "resumed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("on-stop message", "stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("on-destroy message", "destroyed");
+    }
 }
