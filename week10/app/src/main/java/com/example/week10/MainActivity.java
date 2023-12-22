8bbd91acb3e44041b124bd5d3fe58cfea033bc27
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-12-22 10:55:20 +0300

    week 10 init commit

---
+++ week10/app/src/main/java/com/example/week10/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.week10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("start message", "started");
+    }
+
+}
