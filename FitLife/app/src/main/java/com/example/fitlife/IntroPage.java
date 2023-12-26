c2d8d89847eb941b375331d22acc96ca15e7e5ea
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-12-26 14:30:36 +0300

    Main and Intro page complete, working on dashboard

--- FitLife/app/src/main/java/com/example/fitlife/IntroPage.java
+++ FitLife/app/src/main/java/com/example/fitlife/IntroPage.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro_page);
     }
 
     public void nextActivityHandler(View view) {
-        Intent intent = new Intent(MainActivity.this, IntroPage.class);
+        Intent intent = new Intent(IntroPage.this, Dashboard.class);
         startActivity(intent);
     }
 }
